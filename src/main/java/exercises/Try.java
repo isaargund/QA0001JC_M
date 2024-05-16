2a84208812e5f1d8b495170915720b061d8303a3
isa <ode121@gmail.com>
2024-05-16 12:14:48 +0200

    16-05-24v2

--- src/main/java/exercises/Try.java
+++ src/main/java/exercises/Try.java
@@ -1,21 +1,25 @@
 package exercises;
 
+import java.util.ArrayList;
+
 public class Try {
     public static void main(String[] args) {
-        int i=1;
-        do {
-            if (i==3){
-                continue;
-            }else if(i==5){
-                System.out.println(i+" ");
-                break;
 
-            }else {
-                System.out.println(i+" ");
+        //Create an Arraylist as Integer and change odd elements with 10.....
+// 23 , 24, 12, 18,9
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(23);
+        list.add(24);
+        list.add(12);
+        list.add(18);
+        list.add(9);
+
+        int length=list.size();
+        for (int i=0; i<length; i++) {
+            if(list.get(i)%2!=0){
+                list.set(i,10);
+
             }
-            i++;
-        }while (true);
-
-    }
-
-}
+        }
+        System.out.println(list);
+}}
