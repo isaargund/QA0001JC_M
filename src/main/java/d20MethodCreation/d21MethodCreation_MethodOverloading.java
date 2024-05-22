00b3bac877f0273df408ac906854ec2a5f53bfa7
isa <ode121@gmail.com>
2024-05-22 12:07:45 +0200

    22.05.24

---
+++ src/main/java/d20MethodCreation/d21MethodCreation_MethodOverloading.java
@@ -0,0 +1,7 @@
+package d20MethodCreation;
+
+public class d21MethodCreation_MethodOverloading {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/exercises/yy.java
+++ src/main/java/exercises/yy.java
@@ -7,28 +7,24 @@ import java.util.List;
 
 public class yy {
     public static void main(String[] args) {
-        //Example; Create an integer list
-//find the closest element in the list
-// [12,23,10,19]  ==> 10 and 12  ===> 12-10=2
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(12);
-        list.add(23);
-        list.add(10);
-        list.add(19);
-
-        System.out.println(list);//[12, 23, 10, 19]
-        System.out.println("==============================================================0");
-            list.sort(null);
-        System.out.println(list);//[10, 12, 19, 23]
-        System.out.println("==============================================================0");
 
 
-        Collections.sort(list);
-        System.out.println(list);//[10, 12, 19, 23]
-
-
-
+    int i=multiplication(12,10,10);
+        System.out.println(i);
+        str("merhaba");
 
 
     }
+    public  static int multiplication(int mult1, int mult2,int addition) {
+        int total=mult1*mult2+addition;
+        return total;
+    }
+    public  static String str(String text) {
+
+        System.out.println(text);
+        return text;
+
+
+
+}
 }
