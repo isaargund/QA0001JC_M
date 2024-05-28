1daf78823535f2857fc8a999b713276cc4680df5
isa <ode121@gmail.com>
2024-05-28 12:14:13 +0200

    v3

--- src/main/java/d24InstanceBlock_Oop/InstanceBlockRunner.java
+++ src/main/java/d24InstanceBlock_Oop/InstanceBlockRunner.java
@@ -5,6 +5,7 @@ public class InstanceBlockRunner {
         InstanceBlock02 obj1=new InstanceBlock02();
         InstanceBlock02 obj2=new InstanceBlock02(2025);
         InstanceBlock02 obj3=new InstanceBlock02("White","Benzin");
+        //just for git written
 
     }
 }
