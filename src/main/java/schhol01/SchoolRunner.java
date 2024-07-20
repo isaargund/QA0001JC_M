e206f52dfe66c12e803695ebe4860f5ade5b75b9
isa <ode121@gmail.com>
2024-07-20 18:11:29 +0200

    3

--- src/main/java/schhol01/SchoolRunner.java
+++ src/main/java/schhol01/SchoolRunner.java
@@ -1,2 +1,10 @@
-package schhol01;public class SchoolRunner {
+package schhol01;
+
+public class SchoolRunner {
+    public static void main(String[] args) {
+        Teacher01 teacher001=new Teacher01("isa","argundogan","history");
+        System.out.println("teacher001.surname = " + teacher001.surname);
+        Teacher01 teacher002=new Teacher01("nasreddin","dağ","sosyalbilgiler");
+        System.out.println("teacher002 = " + teacher002);
+    }
 }
--- src/main/java/schhol01/Teacher01.java
+++ src/main/java/schhol01/Teacher01.java
@@ -1,2 +1,24 @@
-package schhol01;public class Teacher01 {
+package schhol01;
+
+public class Teacher01 {
+    String name = "No Same information";
+    String surname="No Surname information";
+
+    @Override
+    public String toString() {
+        return "Teacher01{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", Branch='" + Branch + '\'' +
+                '}';
+    }
+
+    String Branch="No Branch information";
+
+    Teacher01(String name, String surname, String Branch){
+        this.name = name;
+        this.surname = surname;
+        this.Branch = Branch;
+
+    }
 }
