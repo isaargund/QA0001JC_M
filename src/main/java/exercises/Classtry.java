789eb771ca539c7196464f574750ae35f581c138
isa <ode121@gmail.com>
2024-07-20 17:24:47 +0200

    1

---
+++ src/main/java/exercises/Classtry.java
@@ -0,0 +1,18 @@
+package exercises;
+
+public final class Classtry {
+
+        private  String myvar;
+
+        public Classtry (final String initialValue){
+            this.myvar = initialValue;
+        }
+
+    public String getMyvar() {
+        return myvar;
+    }
+
+    public String getValue (){
+            return this.myvar;
+        }
+}
